b44d139909fd6a6513e04ed2a4a2863e38d1aca8
mpak <starovoitov.anton@gmail.com>
2017-10-30 23:55:22 +0500

    убрал функцию среза

--- coursera.py
+++ coursera.py
@@ -1,7 +1,23 @@
+from lxml import etree
+import requests
+import openpyxl
+import bs4
 
 
-def get_courses_list():
-    pass
+def get_xml_from_sitemap():
+    response = requests.get('https://www.coursera.org/sitemap~www~courses.xml')
+    xml_data = response.content
+    print(xml_data)
+    return xml_data
+
+
+def get_courses_list(xml):
+    tree = etree.XML(xml)
+    nodes = tree.xpath('/urlset/url/loc')  # Открываем раздел
+    for node in nodes:  # Перебираем элементы
+        print(node.tag, node.keys(), node.values())
+        print('name =', node.get('name'))  # Выводим параметр name
+        print('text =', [node.text])  # Выводим текст элемента
 
 
 def get_course_info(course_slug):
@@ -13,4 +29,5 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    pass
+    courses_xml = get_xml_from_sitemap()
+    courses_list = get_courses_list(courses_xml)
