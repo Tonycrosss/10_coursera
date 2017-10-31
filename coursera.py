5e9824a5b90d1da5b60fee9a0410b137b22c439f
mpak <starovoitov.anton@gmail.com>
2017-10-31 23:42:48 +0500

    Сохранение информации в эксель

--- coursera.py
+++ coursera.py
@@ -1,7 +1,8 @@
 from lxml import etree
 import requests
-import openpyxl
+from openpyxl import Workbook
 from bs4 import BeautifulSoup
+
 
 def get_xml_from_sitemap():
     response = requests.get('https://www.coursera.org/sitemap~www~courses.xml')
@@ -17,27 +18,51 @@ def get_courses_list(xml, quantity):
     return courses_list[:quantity]
 
 
-def get_course_info(course_slug):
-    response = requests.get(course_slug)
-    dirty_html = response.content
-    soup = BeautifulSoup(dirty_html, 'html.parser')
-    course_name = soup.find_all('h2')[0].get_text()
-    course_language = soup.find_all('div', 'rc-Language')[0].get_text()
-    course_start_date = soup.find_all('div', 'startdate')[0].get_text()
-    course_length = len(soup.find_all('div', 'week'))
-    try:
-        course_ratings = soup.find_all('div', 'ratings-text')[0].get_text()
-    except IndexError:
-        course_ratings = 'No ratings yet'
+def get_course_info(courses_url_list):
+    courses_info = []
+    for course in courses_url_list:
+        response = requests.get(course)
+        dirty_html = response.content
+        soup = BeautifulSoup(dirty_html, 'html.parser')
+        course_name = soup.find_all('h2')[0].get_text()
+        course_language = soup.find_all('div', 'rc-Language')[0].get_text()
+        course_start_date = soup.find_all('div', 'startdate')[0].get_text()
+        course_length = len(soup.find_all('div', 'week'))
+        try:
+            course_ratings = soup.find_all('div', 'ratings-text')[0].get_text()
+        except IndexError:
+            course_ratings = 'No ratings yet'
+        courses_info.append({'course_name': course_name,
+                             'course_language': course_language,
+                             'course_start_date': course_start_date,
+                             'course_length': course_length,
+                             'course_ratings': course_ratings})
+    return courses_info
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def output_courses_info_to_xlsx(courses_info):
+    wb = Workbook()
+    ws1 = wb.active
+    ws1.title = 'lalala'
+    ws1['A1'] = 'Название курса'
+    ws1['B1'] = 'Язык курса'
+    ws1['C1'] = 'Дата начала курса'
+    ws1['D1'] = 'Длительность курса (недели)'
+    ws1['E1'] = 'Средняя оценка курса'
+    row_nums = [row_num for row_num in range(2, len(courses_info) + 2)]
+    for course in courses_info:
+        row_num = row_nums.pop()
+        ws1['A{}'.format(row_num)] = course['course_name']
+        ws1['B{}'.format(row_num)] = course['course_language']
+        ws1['C{}'.format(row_num)] = course['course_start_date']
+        ws1['D{}'.format(row_num)] = course['course_length']
+        ws1['E{}'.format(row_num)] = course['course_ratings']
+    wb.save(filename='./courses.xlsx')
 
 
 if __name__ == '__main__':
-    # courses_xml = get_xml_from_sitemap()
-    # courses_quantity = 10
-    # courses_list = get_courses_list(courses_xml, courses_quantity)
-    # print(courses_list)
-    get_course_info('https://www.coursera.org/learn/gamification')
+    courses_xml = get_xml_from_sitemap()
+    courses_quantity = 10
+    courses_list = get_courses_list(courses_xml, courses_quantity)
+    courses_info = get_course_info(courses_list)
+    output_courses_info_to_xlsx(courses_info)
