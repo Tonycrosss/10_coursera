2a20a258f6d610baeee03b0eb4471f0fc1943bd4
mpak <starovoitov.anton@gmail.com>
2017-10-31 22:09:13 +0500

    Вывод списка ссылок на курсы по кол-ву

--- coursera.py
+++ coursera.py
@@ -7,20 +7,15 @@ import bs4
 def get_xml_from_sitemap():
     response = requests.get('https://www.coursera.org/sitemap~www~courses.xml')
     xml_data = response.content
-    print(str(xml_data.decode('utf-8')))
     return xml_data
 
 
-def get_courses_list(xml):
-    tree = etree.XML(xml)
-    # child = tree.getchildren()
-    # for c in tree:
-    #     print(c.text)
-    # nodes = child.xpath('/url/loc')  # Открываем раздел
-    # for node in nodes:  # Перебираем элементы
-    #     print(node.tag, node.keys(), node.values())
-    #     print('name =', node.get('name'))  # Выводим параметр name
-    #     print('text =', [node.text])  # Выводим текст элемента
+def get_courses_list(xml, quantity):
+    tree = etree.fromstring(xml)
+    courses_list = []
+    for item in tree:
+        courses_list.append(item[0].text)
+    return courses_list[:quantity]
 
 
 def get_course_info(course_slug):
@@ -33,4 +28,6 @@ def output_courses_info_to_xlsx(filepath):
 
 if __name__ == '__main__':
     courses_xml = get_xml_from_sitemap()
-    courses_list = get_courses_list(courses_xml)
+    courses_quantity = 10
+    courses_list = get_courses_list(courses_xml, courses_quantity)
+    print(courses_list)
