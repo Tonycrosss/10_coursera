c2748731499c6e9b777de4b193e3cbc6006132a6
mpak <starovoitov.anton@gmail.com>
2017-10-31 23:10:32 +0500

    Сбор информации по курсу

--- coursera.py
+++ coursera.py
@@ -1,8 +1,7 @@
 from lxml import etree
 import requests
 import openpyxl
-import bs4
-
+from bs4 import BeautifulSoup
 
 def get_xml_from_sitemap():
     response = requests.get('https://www.coursera.org/sitemap~www~courses.xml')
@@ -19,7 +18,17 @@ def get_courses_list(xml, quantity):
 
 
 def get_course_info(course_slug):
-    pass
+    response = requests.get(course_slug)
+    dirty_html = response.content
+    soup = BeautifulSoup(dirty_html, 'html.parser')
+    course_name = soup.find_all('h2')[0].get_text()
+    course_language = soup.find_all('div', 'rc-Language')[0].get_text()
+    course_start_date = soup.find_all('div', 'startdate')[0].get_text()
+    course_length = len(soup.find_all('div', 'week'))
+    try:
+        course_ratings = soup.find_all('div', 'ratings-text')[0].get_text()
+    except IndexError:
+        course_ratings = 'No ratings yet'
 
 
 def output_courses_info_to_xlsx(filepath):
@@ -27,7 +36,8 @@ def output_courses_info_to_xlsx(filepath):
 
 
 if __name__ == '__main__':
-    courses_xml = get_xml_from_sitemap()
-    courses_quantity = 10
-    courses_list = get_courses_list(courses_xml, courses_quantity)
-    print(courses_list)
+    # courses_xml = get_xml_from_sitemap()
+    # courses_quantity = 10
+    # courses_list = get_courses_list(courses_xml, courses_quantity)
+    # print(courses_list)
+    get_course_info('https://www.coursera.org/learn/gamification')
